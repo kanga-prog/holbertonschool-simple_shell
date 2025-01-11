50f6a35fe3dee379e98b25332172c37178e493ff
wlfrd18 <wilfriedpanol@gmail.com>
2025-01-11 16:19:08 +0000

    delete the function that execute pipe

--- parse_and_execute.c
+++ parse_and_execute.c
@@ -9,28 +9,12 @@
  */
 void parse_and_execute(char *input)
 {
-	char *command1;
-	char *command2;
-
-	/* Get the first command */
-	command1 = strtok(input, "|");
-
-	while (command1 != NULL)
-	{
-		/* Check if there's a pipe and get the second command */
-		command2 = strtok(NULL, "|");
-
-		if (command2 != NULL)
-		{
-			execute_pipe(command1, command2);
-		}
-		else
-		{
+			char *token;
 			char *args[MAX_ARGS];
 			int i = 0;
 
 			/* Tokenize the command and arguments */
-			char *token = strtok(command1, " ");
+			token = strtok(input, " ");
 
 			while (token != NULL)
 			{
@@ -41,8 +25,4 @@ void parse_and_execute(char *input)
 			args[i] = NULL;
 
 			execute_command(args[0], args, STDIN_FILENO, STDOUT_FILENO);
-		}
-
-		command1 = strtok(NULL, "|");
-	}
 }
